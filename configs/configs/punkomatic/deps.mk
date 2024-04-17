e85f2b3d789333fa076e70ad3df746552bcddb2f
Ilia Pozdnyakov <iliazeus@proton.me>
2024-04-17 19:24:41 +0500

    use lame as encoder

---
+++ configs/configs/punkomatic/deps.mk
@@ -0,0 +1,2 @@
+build/ffmpeg-$(FFMPEG_VERSION)/build-base-punkomatic/ffbuild/config.mak: build/inst/base/lib/libmp3lame.a
+build/ffmpeg-$(FFMPEG_VERSION)/build-thr-punkomatic/ffbuild/config.mak: build/inst/thr/lib/libmp3lame.a
